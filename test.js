e55871b4038e7bdcc6a5c1d2fcd55b7a135f02e0
Farid El-Aouadi <faridelaouadi@hotmail.co.uk>
2019-11-15 13:53:23 +0000

    modal that has scrolling gifs using giphy api has been added

--- test.js
+++ test.js
@@ -2,13 +2,18 @@
 var request = new XMLHttpRequest()
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=ZsDhKYMNVtzRxcbGwrOBKhNElDE4ZRDv&q=nav&limit=1&offset=0&rating=G&lang=en', true)
+request.open('GET', 'https://api.giphy.com/v1/gifs/search?api_key=ZsDhKYMNVtzRxcbGwrOBKhNElDE4ZRDv&q=nav&limit=25&offset=0&rating=G&lang=en', true)
 
 request.onload = function() {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
-    console.log(data["data"][0]["images"]["hd"]["mp4"])
+    //console.log(data["data"][0]["images"]["hd"]["mp4"])
+    data["data"].forEach(value => {
+      var img = document.createElement('img');
+      img.src =  value["images"]["downsized_medium"]["url"];
+      document.getElementById("modalBody").appendChild(img);
+    })
 }
 };
 
